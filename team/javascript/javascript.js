2d3b9599b4f6536718acbc937f17b25e83d5c2ce
Abolfazl-drx <abolfazl_drx@yahoo.com>
2021-10-08 05:22:54 -0700

    team-tab v02

--- team/javascript/javascript.js
+++ team/javascript/javascript.js
@@ -1,24 +1,59 @@
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-const move_menu=document.querySelector(".container");
-const on_Click=document.querySelector(".container");
-menuBtn.onclick = () => {
-    navbar.classList.add("show");
-    body.classList.add("disabled");
-    move_menu.classList.add("move-menu")
+
+function openMenu() {
+    let menoBox = document.getElementById("hambergure-content");
+
+
+    menoBox.classList.add("open-menu");
+    menoBox.classList.remove("close-menu");
+
+    let bodyBox = document.getElementById("container");
+
+    bodyBox.classList.add("moveOn-container");
+    bodyBox.classList.remove("moveBack-container");
+
+
+    let menunav = document.getElementById('hambergure-menu')
+
+    menunav.classList.add("moveOn-container");
+    menunav.classList.remove("moveBack-container");
 }
-cancelBtn.onclick = () => {
-    body.classList.remove("disabled");
-    navbar.classList.remove("show");
-    move_menu.classList.remove("move-menu")
+function closeMenu() {
+    let menoBox = document.getElementById("hambergure-content");
+    menoBox.classList.remove("open-menu");
+    menoBox.classList.add("close-menu");
+
+    let bodyBox = document.getElementById("container");
+    bodyBox.classList.remove("moveOn-container");
+    bodyBox.classList.add("moveBack-container");
+
+    let menunav = document.getElementById('hambergure-menu')
+
+    menunav.classList.remove("moveOn-container");
+    menunav.classList.add("moveBack-container");
+
 }
-on_Click.onclick = () => {
-    navbar.classList.remove("show");
-    move_menu.classList.remove("move-menu")
+function closeMenuBody() {
+    let menoBox = document.getElementById("hambergure-content");
+    let bodyBox = document.getElementById("container");
+    let menunav = document.getElementById('hambergure-menu')
+
+
+    if (menoBox.classList.contains("open-menu")){
+        menoBox.classList.remove("open-menu");
+        menoBox.classList.add("close-menu");
+        bodyBox.classList.remove("moveOn-container");
+        bodyBox.classList.add("moveBack-container");
+        menunav.classList.remove("moveOn-container");
+        menunav.classList.add("moveBack-container");
+    }
 }
 window.addEventListener("scroll", function () {
-    let navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("sticky", window.scrollY > 0);
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+})
+
+
+window.addEventListener("scroll", function () {
+    let scrollmenu = document.querySelector("#hambergure-menu");
+    scrollmenu.classList.toggle("sticky", window.scrollY > 110);
 })
